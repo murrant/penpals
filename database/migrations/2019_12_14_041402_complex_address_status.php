ecdda46957f7ed0f650c58ccb1fbcc8c93005f19
Tony Murray <murraytony@gmail.com>
2019-12-15 08:25:09 -0600

    drop default value constraint for sqlsrv

--- database/migrations/2019_12_14_041402_complex_address_status.php
+++ database/migrations/2019_12_14_041402_complex_address_status.php
@@ -13,7 +13,13 @@ class ComplexAddressStatus extends Migration
      */
     public function up()
     {
-        Schema::table('addresses', function (Blueprint $table) {
+        $tableName = 'addresses';
+        if ('sqlsrv' == DB::connection()->getDriverName()) {
+            $defaultContraint = DB::selectOne("SELECT OBJECT_NAME([default_object_id]) AS name FROM SYS.COLUMNS WHERE [object_id] = OBJECT_ID('[dbo].[$tableName]') AND [name] = 'valid'");
+            DB::statement("ALTER TABLE [dbo].[$tableName] DROP CONSTRAINT $defaultContraint->name");
+        }
+
+        Schema::table($tableName, function (Blueprint $table) {
             $table->dropColumn('valid');
             $table->unsignedSmallInteger('status')->default(AddressStatus::Unverified);
         });
