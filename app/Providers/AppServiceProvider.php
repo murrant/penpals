24c3bc9074695979f364df68478ad7999ec10e58
Tony Murray <murraytony@gmail.com>
2019-12-15 10:00:59 -0600

    totally remove

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-use Doctrine\DBAL\Schema\Index;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\ServiceProvider;
-use Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,24 +24,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-    }
-
-    private function registerMacros()
-    {
-        Blueprint::macro('dropDefaultConstraint', function (string $column) {
-            $connection = Schema::getConnection();
-            if ($connection->getDriverName() === 'sqlsrv') {
-                $query = sprintf(
-                    'SELECT OBJECT_NAME([default_object_id]) AS name ' .
-                    'FROM SYS.COLUMNS ' .
-                    "WHERE [object_id] = OBJECT_ID('[dbo].[%s]') AND [name] = '%s'",
-                    $this->getTable(),
-                    $column
-                );
-                $result = $connection->selectOne($query);
-                $constraint = new Index($result->name, [$column]);
-                $connection->getDoctrineSchemaManager()->dropConstraint($constraint, $this->getTable());
-            }
-        });
     }
 }
