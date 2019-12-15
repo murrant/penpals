98f0a04301b9778410b2a2bcbc46a3932dfe4b6e
Tony Murray <murraytony@gmail.com>
2019-12-15 09:58:54 -0600

    Disable drop constraint macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerMacros();
+        //
     }
 
     private function registerMacros()
