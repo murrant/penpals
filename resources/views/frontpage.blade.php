ad6be2c080d97a2df08ea1a0ad1061960ad7809b
Tony Murray <murraytony@gmail.com>
2019-12-30 08:48:11 -0600

    icons and meaningful image names

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -13,20 +13,20 @@
         <div class="col"><hr></div>
         <div class="row">
             <div class="col-sm-12 col-md-6 col-lg-4 justify-content-center text-center panel">
-                <img src="{{ asset('images/1.png') }}">
+                <img src="{{ asset('images/mail.png') }}">
                 <h3>Become a Letter Writer</h3>
                 <p>Become a Volunteer and we will set you up with a batch of new pen pals to write to</p>
                 <div><a class="btn btn-primary" href="{{ route('register') }}">SIGN UP</a></div>
             </div>
             <div class="col-sm-12 col-md-6 col-lg-4 justify-content-center text-center panel">
-                <img src="{{ asset('images/2.png') }}">
+                <img src="{{ asset('images/writing.png') }}">
                 <h3>Basic Letter Guidelines</h3>
                 <p>Information and Basic Guidelines to follow for writing letters to your pen pal</p>
                 <div class="justify-content-center"><a class="btn btn-primary" href="{{ route('welcome') }}/">FIND OUT MORE</a>
                 </div>
             </div>
             <div class="col-sm-12 col-md-6 col-lg-4 justify-content-center text-center panel">
-                <img src="{{ asset('images/3.png') }}" class="round-circle">
+                <img src="{{ asset('images/flyer_bw.png') }}" class="round-circle">
                 <h3>Black &amp; White Print Out</h3>
                 <p>Click the Image for Fill In the Blank Or Download for Decorah Iowa</p>
                 <div><a class="btn btn-primary" href="https://drive.google.com/file/d/1C8_KtU5ZoMynzEDn21gXRlqieiXn4jkH/view?usp=sharing">DOWNLOAD</a></div>
