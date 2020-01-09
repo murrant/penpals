6ca703be5e28e84fc9804325100a7a5618c36107
Tony Murray <murraytony@gmail.com>
2020-01-08 20:13:01 -0600

    Request count

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,7 +73,7 @@
                         @endcan
                         @can('approve-requests')
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('address-request.index') }}">{{ __('Approve Requests') }}</a>
+                                <a class="nav-link" href="{{ route('address-request.index') }}">{{ __('Approve Requests') }} ({{ \App\AddressRequest::count() }})</a>
                             </li>
                         @endcan
 
