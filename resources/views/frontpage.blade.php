e9090d272d96fa02fc7446c2be15d0f52047e584
Tony Murray <murraytony@gmail.com>
2020-01-12 22:48:13 -0600

    Video on front page and better column layout

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -36,25 +36,31 @@
         <div class="col">
             <hr>
         </div>
-        <div class="row">
-            <div class="col-sm-12 col-md-6 col-lg-4 justify-content-center text-center panel">
-                <img src="{{ asset('images/mail.png') }}">
+        <div class="row row-eq-height">
+            <div class="col-sm-12 col-md-6 col-lg-4 text-center panel mt-2">
+                <img class="img-fluid" src="{{ asset('images/mail.png') }}">
                 <h3>Become a Letter Writer</h3>
                 <p>Become a Volunteer and we will set you up with a batch of new pen pals to write to</p>
                 <div><a class="btn btn-primary" href="{{ route('register') }}">SIGN UP</a></div>
             </div>
-            <div class="col-sm-12 col-md-6 col-lg-4 justify-content-center text-center panel">
-                <img src="{{ asset('images/writing.png') }}">
+            <div class="col-sm-12 col-md-6 col-lg-4 text-center panel mt-2">
+                <img class="img-fluid" src="{{ asset('images/writing.png') }}">
                 <h3>Basic Letter Guidelines</h3>
                 <p>Information and Basic Guidelines to follow for writing letters to your pen pal</p>
                 <div class="justify-content-center"><a class="btn btn-primary" href="{{ route('welcome') }}/">FIND OUT MORE</a>
                 </div>
             </div>
-            <div class="col-sm-12 col-md-6 col-lg-4 justify-content-center text-center panel">
-                <img src="{{ asset('images/flyer_bw.png') }}" class="round-circle">
-                <h3>Black &amp; White Print Out</h3>
-                <p>Click the Image for Fill In the Blank Or Download for Decorah Iowa</p>
-                <div><a class="btn btn-primary" href="https://drive.google.com/file/d/1C8_KtU5ZoMynzEDn21gXRlqieiXn4jkH/view?usp=sharing">DOWNLOAD</a></div>
+            <div class="col-sm-12 col-md-6 col-lg-4 text-center panel d-flex flex-column mt-2">
+                <div class="flex-grow-1 d-flex">
+                    <div class="embed-responsive embed-responsive-16by9 align-self-center">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/uG5I-32sG20" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div>
+                    <h3>Training Video</h3>
+                    <p>This video will go through everything you need to be able be successful</p>
+                    <div><a class="btn btn-primary" href="https://drive.google.com/file/d/1C8_KtU5ZoMynzEDn21gXRlqieiXn4jkH/view?usp=sharing">DOWNLOAD</a></div>
+                </div>
             </div>
         </div>
     </div>
